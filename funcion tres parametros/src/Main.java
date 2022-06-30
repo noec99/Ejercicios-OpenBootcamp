cbf347932ccb430b29d2040db33a5459e13f59ac
Noe <noec99@hotmail.com>
2022-06-29 19:27:41 -0500

    suma tres parametros

---
+++ funcion tres parametros/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    static int Suma(int a, int b, int c){
+        return a+b+c;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Suma(2,3,4));
+
+
+    }
+}
